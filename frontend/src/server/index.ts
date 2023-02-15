ab3d00b563377c703f58b9588483a0d688a6bc7e
李云飞 <liyunfei02@daojia-inc.com>
2023-02-15 18:34:49 +0800

    feat: create order

--- frontend/src/server/index.ts
+++ frontend/src/server/index.ts
@@ -10,21 +10,37 @@ import PBP_Address from '~/../../contracts/abi/pbarter_protocol_address';
 import Web3 from "web3";
 // web3 instance
 const web3 = new Web3("https://rpc.testnet.moonbeam.network");
-const myAccount = window.localStorage.getItem('account') || '';
 // Contract
 const SFTS_CONT = new web3.eth.Contract(SFTs_ABI, SFTS_Address)
 const NFTS_CONT = new web3.eth.Contract(NFTS_ABI, NFTS_Address)
 const PBP_CONT = new web3.eth.Contract(PBP_ABI, PBP_Address)
-
-export async function getSFTS_NFTS() {
-  const sum = await SFTS_CONT.methods.balanceOf(myAccount).call();
+export const ContractList = [
+  {
+    name: '3525',
+    value: 3525,
+    address: SFTS_Address,
+    key: 1,
+  },
+  {
+    name: '721',
+    value: 721,
+    address: NFTS_Address,
+    key: 2,
+  }
+]
+export async function getNFTS(contract: any) {
+  const myAccount = window.localStorage.getItem('account') || '';
+  const sum = await contract.methods.balanceOf(myAccount).call();
   console.log(sum, myAccount)
-  const nfts: never[] = [];
-  // for (let i = 0; i++; i < sum) {
-  //   const res = await incrementer.methods.tokenOfOwnerByIndex(myAccount, i).call()
-  //   console.log('res', res)
-  //   nfts.push(res)
-  // }
-  console.log('nfts', nfts)
+  const nfts = [];
+  for (let i = 0; i++; i < sum) {
+    const res = await contract.methods.tokenOfOwnerByIndex(myAccount, i).call()
+    nfts.push(res)
+  }
   return nfts;
+}
+export async function createOrder(baseAddr: string, targetAddr: string, baseId: string, targetId: string) {
+  const myAccount = window.localStorage.getItem('account') || '';
+  const res = await PBP_CONT.methods.createOrder(baseAddr, targetAddr, [baseId], [targetId]).call();
+  return res;
 }
