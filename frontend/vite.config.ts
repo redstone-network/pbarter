b7c781f15e4d45a4d10342ee66617c8579ed0975
todaylab <jinrishouji@sina.com>
2023-02-17 18:29:46 +0800

    fix yarn build Top-level await is not available in the configured target environment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()]
+  plugins: [svgr(), react(), tsconfigPaths()],
+  build: {
+    target: 'esnext'
+  }
 })
